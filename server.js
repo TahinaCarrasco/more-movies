a2bf1c3381f1a328e246da71fbe76dedb577e8a2
TAHINA PEREIRA DA SILVA <tahina.silva@viavarejo.com.br>
2018-09-05 15:03:56 -0300

    test

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/more-movies'));
+app.use(express.static(__dirname + '/app/dist/more-movies'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/more-movies/index.html'));
+res.sendFile(path.join(__dirname+'/app/dist/more-movies/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
